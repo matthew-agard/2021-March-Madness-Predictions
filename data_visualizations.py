b6f4ad2e075f988a8976eb30e919a0c06c4f2b1d
Matthew Agard <matthew.agard01@gmail.com>
2021-03-04 00:25:22 -0600

    Modularize null imputation code, Selected ML Models

--- data_visualizations.py
+++ data_visualizations.py
@@ -1,4 +1,15 @@
+import pandas as pd
 import matplotlib.pyplot as plt
+
+
+def feature_null_counts(df):
+    nulls = df.isnull().sum().sort_values(ascending=False)
+    return nulls[nulls > 0]
+
+
+def get_null_rows(null_fills, df):
+    rows = df[df[null_fills].isnull().any(axis=1)]
+    return rows[['Year'] + null_fills]
 
 
 def format_plot(title, xlabel, ylabel):
